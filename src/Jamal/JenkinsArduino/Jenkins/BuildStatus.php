a99c6f5d008e86856c7c16b4b61351a9c9bc5d39
Caio Almeida <caio.almeida@catho.com>
2015-12-28 09:46:51 -0200

    criando entidade build status

---
+++ src/Jamal/JenkinsArduino/Jenkins/BuildStatus.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Jamal\JenkinsArduino\Jenkins;
+
+/**
+ * @author Caio Almeida <caioamd@hotmail.com>
+ */
+class BuildStatus
+{
+    const BUILDING  = 'BUILDING';
+    const SUCCESS   = 'SUCCESS';
+    const FAILURE   = 'FAILURE';
+    const UNSTABLE  = 'UNSTABLE';
+    const ABORTED   = 'ABORTED';
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+    /**
+     * @param string $status
+     */
+    public function __construct($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCode()
+    {
+        $codes = [
+            self::BUILDING  => 0,
+            self::FAILURE   => 1,
+            self::SUCCESS   => 2,
+            self::UNSTABLE  => 3,
+            self::ABORTED   => 4
+        ];
+
+        return $codes[$this->status];
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->status;
+    }
+}
