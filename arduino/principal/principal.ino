eddfa9553619d4eb3b8cb7355665f9908c0e9979
Caio Almeida <caio.f.r.amd@gmail.com>
2016-01-29 15:39:11 -0200

    configurando constantes de status

--- arduino/principal/principal.ino
+++ arduino/principal/principal.ino
@@ -10,6 +10,16 @@
 #define BUZZER 6
 
 /**
+ * Definição dos códigos do status do build.
+ */
+#define STATUS_BUILD_FAIL 1
+#define STATUS_BUILD_SUCCESS 2
+#define STATUS_BUILD_UNSTABLE 3
+#define STATUS_BUILD_ABORTED 4
+#define STATUS_BUILD_RUNNING 0
+#define STATUS_BUILD_SHUTDOWN 9
+
+/**
  * @var LiquidCrystral_I2C
  */
 LiquidCrystal_I2C lcd(0x27, 16, 2);
