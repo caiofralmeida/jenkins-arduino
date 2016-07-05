bbe3c09f7f274a127ad94f0a99f840d933df7d31
Caio Almeida <caio.f.r.amd@gmail.com>
2016-07-04 21:44:22 -0300

    Corrigindo identação

--- src/JenkinsArduino/Serial/Writer.php
+++ src/JenkinsArduino/Serial/Writer.php
@@ -6,25 +6,25 @@ namespace Jamal\JenkinsArduino\Serial;
  * @author Caio Almeida <caioamd@hotmail.com>
  * @author Gustavo Lira <gustavolira1506@hotmail.com>
  */
- class Writer implements Writable
+class Writer implements Writable
+{
+ /**
+  * @var string
+  */
+ const MODE = 'w';
+
+ /**
+  * @param  string $path
+  * @param  mixed $data
+  * @return void
+  */
+ public function write($path, $data)
  {
-     /**
-      * @var string
-      */
-     const MODE = 'w';
-
-     /**
-      * @param  string $path
-      * @param  mixed $data
-      * @return void
-      */
-     public function write($path, $data)
-     {
-         if (!$resource = @fopen($path, self::MODE)) {
-             throw new CannotWriteException();
-         }
-
-         fwrite($resource, $data);
-         fclose($resource);
+     if (!$resource = @fopen($path, self::MODE)) {
+         throw new CannotWriteException();
      }
+
+     fwrite($resource, $data);
+     fclose($resource);
  }
+}
